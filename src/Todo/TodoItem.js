fe42afef962c6f018452f053f8be910ab37b5809
TadjievCodes <muhammad2008.08@mail.ru>
2021-04-28 01:26:44 -0400

    some styles added for the form

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { findByLabelText } from '@testing-library/dom';
+
+
+const styles = {
+   li: {
+   display: 'flex',
+   justifyContent: 'space-between',
+   alignITems: 'center',
+   padding: '.5rem 1rem',
+   border: '1px solid #ccc',
+   borderRadius: '4px',
+   marginBottom: '.5rem'
+
+   },
+   input: {
+    marginRight: '1rem'
+   }
+}
+
 
 function TodoItem({ todo, index }) {
     return(
-       <li> 
+       <li style={styles.li}> 
          <span>
-            <input type='checkbox'  />
+            <input type='checkbox'  style={styles.input} />
             <strong>{index + 1}</strong>
             {todo.title}
         </span>
